4ec631360186c296b349e0c8dab5ebee78a779ee
Osanda Deshan <osanda.deshan@gmail.com>
2021-04-16 16:47:00 +0530

    Code refactored.

--- app/src/main/java/com/maxsoftlk/meetosanda/MainActivity.java
+++ app/src/main/java/com/maxsoftlk/meetosanda/MainActivity.java
@@ -2,29 +2,27 @@ package com.maxsoftlk.meetosanda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    private WebView mywebview;
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mywebview = (WebView) findViewById(R.id.webView);
-        WebSettings webSettings = mywebview.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        mywebview.loadUrl("https://osandadeshan.github.com/");
-        mywebview.setWebViewClient(new WebViewClient());
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(getString(R.string.app_url));
+        webView.setWebViewClient(new WebViewClient());
     }
 
     @Override
     public void onBackPressed() {
-        if (mywebview.canGoBack()) {
-            mywebview.goBack();
+        if (webView.canGoBack()) {
+            webView.goBack();
         } else {
             super.onBackPressed();
         }
